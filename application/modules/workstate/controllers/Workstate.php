06f4f87af37613fce4ec2f87adc8451a571c1f2b
Mukhlis Wicaksono <aduka.last@gmail.com>
2022-07-19 10:42:52 +0700

    init commit

---
+++ application/modules/workstate/controllers/Workstate.php
@@ -0,0 +1,237 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Workstate extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+	}
+	public function index()
+	{	
+		$objForm = new WorkstateViewModel();
+		
+		// Paging Variable
+        $pagesize = 0;
+        $rowcount = 0;
+        $selectedindexrow = 1;
+
+        // Filter Variable
+        $search;
+        $workstatetypeid;
+		
+        // Detect From Postback
+        $FilterSubmit = isset($_POST["FilterSubmit"]) ? $_POST["FilterSubmit"] : 0;
+
+        // Get Data Form Postback
+        if ($FilterSubmit == 1){
+	        // Get Filter Parameter
+            if ($objForm->Form == null)
+            {
+                $search = "";
+                $workstatetypeid = 0;
+            }else{
+				if(strlen($objForm->Form->Search) != 0){
+					$search = $objForm->Form->Search;
+				}else{
+					$search = "";
+				}
+				$workstatetypeid = $objForm->Obj->WorkstateTypeID;
+			}
+			// Get Paging Parameter
+			if ($_POST["PageSize"] != 0)
+			{
+				$pagesize = $_POST["PageSize"];
+			}
+
+			if ($_POST["RowCount"] != 0)
+			{
+				$rowcount = $_POST["RowCount"];
+			}
+
+			if ($_POST["SelectedIndexRow2"] != 0)
+			{
+				$selectedindexrow = $_POST["SelectedIndexRow2"];
+			}
+
+			if ($_POST["First2"] != 0)
+			{
+				$selectedindexrow = $_POST["First2"];
+			}
+
+			if ($_POST["Prev2"] != 0)
+			{
+				$selectedindexrow = $_POST["Prev2"];
+			}
+
+			if ($_POST["Next2"] != 0)
+			{
+				$selectedindexrow = $_POST["Next2"];
+			}
+
+			if ($_POST["Last2"] != 0)
+			{
+				$selectedindexrow = $_POST["Last2"];
+			}
+		}
+		else{
+			$search = "";
+			$workstatetypeid = 0;
+		}
+		
+		// Instance Class Form
+        $objForm->Form = new FormModel();
+
+        // Default PageSize
+		$objForm->Form->PageSize = 20;
+		$objForm->Form->RowCount = 0;
+		
+		// Get PageSize
+		if ($pagesize != 0)
+		{
+			$objForm->Form->PageSize = $pagesize;
+		}
+
+		// Get RowCount
+		if ($rowcount != 0)
+		{
+			$objForm->Form->RowCount = $rowcount;
+		}
+
+		// Order
+		$objForm->Form->SortOrder = $objForm->Form->SortOrder == null ? "Name" : $objForm->Form->SortOrder;
+
+		// Declare URL
+		$objForm->Form->urlBase = $this->URLClass->WorkstateUrl()."search/";
+		$objForm->Form->url = $this->URLClass->WorkstateUrl()."search/";
+
+		// Determine StartRow By Selected Index
+        $objForm->Form->SelectedIndexRow = $selectedindexrow;
+        if ($selectedindexrow > 0)
+        {
+            $temp = $selectedindexrow - 1;
+            $objForm->Form->StartRow = ($temp * $objForm->Form->PageSize) + 1;
+            if ($objForm->Form->url == $objForm->Form->urlBase)
+            {
+                $objForm->Form->url = $objForm->Form->url . "?StartRow=" . $objForm->Form->StartRow;
+            }
+            else
+            {
+                $objForm->Form->url = $objForm->Form->url . "&StartRow=" . $objForm->Form->StartRow;
+            }
+        }
+
+		// Determine StartRow Out of Row Count
+		if ($objForm->Form->RowCount <= $objForm->Form->PageSize)
+		{
+			$objForm->Form->url = $objForm->Form->urlBase;
+			$objForm->Form->StartRow = 1;
+		
+			if ($objForm->Form->url == $objForm->Form->urlBase)
+			{
+				$objForm->Form->url = $objForm->Form->url . "?StartRow=" . $objForm->Form->StartRow;
+			}
+			else
+			{
+				$objForm->Form->url = $objForm->Form->url . "&StartRow=" . $objForm->Form->StartRow;
+			}
+		}
+		
+		// Determine EndRow
+		if ($objForm->Form->PageSize != 0)
+		{
+			$objForm->Form->EndRow = $objForm->Form->StartRow + $objForm->Form->PageSize - 1;
+			if ($objForm->Form->url == $objForm->Form->urlBase)
+			{
+				$objForm->Form->url = $objForm->Form->url . "?EndRow=" . $objForm->Form->EndRow;
+			}
+			else
+			{
+				$objForm->Form->url = $objForm->Form->url . "&EndRow=" . $objForm->Form->EndRow;
+			}
+		}
+		
+		// Search Starting from Selected Index
+		$objForm->Form->Search = trim($search);
+		if (strlen($objForm->Form->Search) != 0)
+		{
+			if ($objForm->Form->url == $objForm->Form->urlBase)
+			{
+				$objForm->Form->url = $objForm->Form->url . "?Search=" . $objForm->Form->Search;
+			}
+			else
+			{
+				$objForm->Form->url = $objForm->Form->url . "&Search=" . $objForm->Form->Search;
+			}
+		}
+		
+		// Instance Object
+		$objForm->Obj = new WorkstateModel();
+				
+		// Instance DropDown Filter 
+		$objForm->Obj->DropDownWorkstateType = $objForm->Obj->PopulateDDWorkstateType();
+
+		$objForm->Obj->WorkstateTypeID = $workstatetypeid;
+		//////////////////// Filter ////////////////////
+		if ($workstatetypeid != 0)
+		{
+			$objForm->Form->url = $objForm->Form->url . "&WorkstateTypeID=" . $objForm->Obj->WorkstateTypeID;
+		}
+		//////////////////// End Filter ////////////////////
+
+		// Get Data
+		$objForm->ObjList = $this->WorkstateModel->Listing($objForm);
+
+		// Search Purpose
+        // Recompose Url with StartRow = 1
+        if ((count($objForm->ObjList) == 0) && ($objForm->Form->StartRow != 1))
+        {
+            $objForm->Form->StartRow = 1;
+            $objForm->Form->EndRow = $objForm->Form->StartRow + $objForm->Form->PageSize - 1;
+            $objForm->Form->url = $objForm->Form->urlBase;
+            $objForm->Form->Search = $objForm->Form->Search == null ? "" : $objForm->Form->Search;
+            if ($objForm->Form->Search != "")
+            {
+                $objForm->Form->url = $objForm->Form->url . "?Search=" . $objForm->Form->Search . "&StartRow=" . $objForm->Form->StartRow . "&EndRow=" . $objForm->Form->EndRow;
+            }
+            else
+            {
+                $objForm->Form->url = $objForm->Form->url . "?StartRow=" . $objForm->Form->StartRow . "&EndRow=" . $objForm->Form->EndRow;
+            }
+
+            //////////////////// Filter ////////////////////
+            if ($workstatetypeid != 0)
+            {
+                $objForm->Obj->WorkstateTypeID = $workstatetypeid;
+                $objForm->Form->url = $objForm->Form->url . "&WorkstateTypeID=" . $objForm->Obj->WorkstateTypeID;
+            }
+            //////////////////// End Filter ////////////////////
+            // Get Data
+            $objForm->ObjList = $this->WorkstateModel->Listing($objForm);
+		}
+		
+		// Page Count
+        $tempCount = $objForm->Form->RowCount / $objForm->Form->PageSize;
+        $objForm->Form->PageCount = ceil($tempCount);
+
+        if ($objForm->Form->PageCount == 0)
+        {
+            $objForm->Form->PageCount = 1;
+        }
+
+        // Set End Row
+        if ($objForm->Form->EndRow > $objForm->Form->RowCount)
+        {
+            $objForm->Form->EndRow = $objForm->Form->RowCount;
+        }
+		// Model.Form.SelectedIndexRow
+		// Model.Form.PageCount
+		// model.Obj.WorkstateTypeID
+		// Model.Obj.DropDownWorkstateType
+		// Model.Form.PageSize
+		// Model.ObjList.Count
+		// Model.ObjList []
+		// Model.Form.StartRow
+		// Model.Form.EndRow
+		// Model.Form.RowCount
+		$this->template->load('v_workstate', $objForm);
+	}
+}
---
+++ application/modules/workstate/views/v_workstate.php
@@ -0,0 +1,309 @@
+<?php 
+    $this->template->set('title','Workstate Listing');
+
+    $Next2 = 0;
+    $Prev2 = 0;
+
+    $Next2 = $Form->SelectedIndexRow + 1;
+    if ($Next2 >= $Form->PageCount)
+    {
+        $Next2 = $Form->PageCount;
+    }
+
+    $Prev2 = $Form->SelectedIndexRow - 1;
+    if ($Prev2 == 0)
+    {
+        $Prev2 = 1;
+    }
+
+    $BannedDetected = "";
+?>
+<!-- <?php var_dump($ObjList);?> -->
+<div class="content-wrapper" document="doc-1column-template">
+    <div class="container" style="min-height:450px;">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-header">Workstate <small>Listing</small></h1>
+            </div> <!--- col --->
+        </div> <!--- row --->
+        <!--- begin form --->
+        <form role="form" action="/CI/Workstate" method="POST" class="form">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-inline search-form">
+                        <div class="control-label hidden-xs">&nbsp;</div>
+                        <div class="input-group">
+                            <!--- search text box --->
+                            <input class="form-control" id="SearchTextBox" name="Form.Search" type="text" value="">
+                            <span class="input-group-btn">
+                                <!--- search button --->
+                                <button type="button" id="fSearchbtn" class="btn btn-default"><span class="glyphicon glyphicon-search"></span><span class="hidden-xs"> Search</span></button>
+                                <!--- reset button --->
+                                <button type="button" id="fResetbtn" class="btn btn-default"><span class="glyphicon glyphicon-refresh"></span><span class="hidden-xs"> Reset</span></button>
+                                <!--- filter button --->
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="col-xs-7">
+                            <!--- Workstate --->
+                            <div class="form-group">
+                                <label class="control-label">Workstate Type</label>
+                                <select class="form-control" data-val="true" data-val-number="The field WorkstateTypeID must be a number." data-val-required="The WorkstateTypeID field is required." id="WorkstateTypeID" name="Obj.WorkstateTypeID">                                
+                                <?php
+                                    for($i=0; $i < count($Obj->DropDownWorkstateType); $i++){
+                                ?>
+                                    <option value="<?= $Obj->DropDownWorkstateType[$i]->WorkstateTypeID; ?>"><?= $Obj->DropDownWorkstateType[$i]->WorkstateTypeName; ?></option>
+                                <?php
+                                    }
+                                ?> 
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xs-5">
+                            <!--- Rows --->
+                            <div class="form-group">
+                                <label class="control-label">Rows</label>
+                                <select id="PageSize" name="PageSize" class="form-control">
+                                    <?php
+                                        // PHP SIDE
+                                        $data = 5;
+                                        $start = 10;
+                                        for ($i = 0; $i < $data; $i++)
+                                        { 
+                                            if($Form->PageSize == $start){
+                                    ?>
+                                                <!-- HTML SIDE -->
+                                                <option value="<?= $start; ?>" selected><?= $start; ?></option>                                                
+                                    <?php
+                                            // PHP SIDE
+                                            }else{
+                                    ?>
+                                                <!-- HTML SIDE -->
+                                                <option value="<?= $start; ?>"><?= $start; ?></option>
+                                    <?php
+                                            // PHP SIDE
+                                            }
+                                            $start = $start + 10;
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             </div> <!-- col -->
+            <!--- collapsible section for filter --->
+            
+            <!--- end of collapsible section for filter --->
+            <!--- display data --->
+            <div class="row">
+                <!--- row --->
+                <div class="col-md-12">
+                    <!--- col--->
+                    <div class="table-responsive">
+                        <table class="table table-condensed">
+                            <thead>
+                                <tr>
+                                    <th class="text-right" style="width:3%">No.</th>
+                                    <th style="width:25%">Name</th>
+                                    <th>Workstate Type</th>
+                                    <th class="text-center">Finalize</th>
+                                    <th class="text-center">Backward Allow</th>
+                                    <th class="text-center">Level</th>
+                                    <th class="text-center">Color</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    for($i=0; $i < count($ObjList); $i++){
+                                ?>
+                                    <tr>
+                                        <td class="text-right"><?= $i+1; ?><text>.</text></td>
+                                        <td><a href="/CI/Workstate/Detail/<?= $ObjList[$i]->ID; ?>"><?= $ObjList[$i]->Name; ?></a><span class="ref-num"> (<?= $ObjList[$i]->ID; ?>)</span></td>
+                                        <td><?= $ObjList[$i]->WorkstateTypeName; ?></td>
+                                        <td class="text-center">
+                                            <?php
+                                            if ($ObjList[$i]->Finalized == 1)
+                                            {
+                                                echo '<span class="glyphicon glyphicon-ok"></span>';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td class="text-center">
+                                            <?php
+                                            if ($ObjList[$i]->BackwardAllow == 1)
+                                            {
+                                                echo '<span class="glyphicon glyphicon-ok"></span>';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td class="text-center"><?= $ObjList[$i]->Level; ?></td>
+                                        
+                                        <td class="text-center"><div class="color-palette" style="background-color: <?= $ObjList[$i]->Color; ?>"><span style="color: rgba(255, 255, 255, 0.8);"> <?= $ObjList[$i]->Color; ?></span></div></td>
+                                    </tr>
+                                <?php
+                                    }
+                                ?>
+                                    
+                            </tbody>
+                        </table>
+                    </div>
+                </div> <!--- col --->
+            </div> <!--- row --->
+            <!--- BOTTOM PAGER --->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="data-pager data-pager-bottom">
+                        <p><?= $Form->StartRow; ?> - <?= $Form->EndRow; ?> of <?= $Form->RowCount; ?></p>
+                        <div class="btn-group">
+                            <button type="submit" class="btn btn-default" id="First2" name="First2" value="1"><span class="glyphicon glyphicon-menu-left"></span><span class="glyphicon glyphicon-menu-left"></span></button>
+                            <button type="submit" class="btn btn-default" id="Prev2" name="Prev2" value="<?= $Prev2; ?>"><span class="glyphicon glyphicon-menu-left"></span></button>
+                            <select id="SelectedIndexRow2" name="SelectedIndexRow2" class="btn">
+                                <?php
+                                    $data2 = $Form->PageCount;
+                                    $start2 = 1;
+                                    for ($i = 0; $i < $data2; $i++)
+                                    {
+                                        if ($Form->SelectedIndexRow == $start2)
+                                        {
+                                ?>
+                                            <option value="<?= $start2; ?>" selected><?= $start2; ?></option>
+                                <?php
+                                        }
+                                        else
+                                        {
+                                ?>
+                                            <option value="<?= $start2; ?>"><?= $start2; ?></option>
+                                <?php
+                                        }
+                                        $start2 = $start2 + 1;
+                                    }
+                                ?>
+                            </select>
+                            <button type="submit" class="btn btn-default" id="Next2" name="Next2" value="<?= $Next2; ?>" role="button"><span class="glyphicon glyphicon-menu-right"></span></button>
+                            <button type="submit" class="btn btn-default" id="Last2" name="Last2" value="<?= $Form->PageCount?>" role="button"><span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-menu-right"></span></button>
+                        </div>
+                    </div>
+                </div> <!--- col--->
+            </div> <!--- row --->
+            <!--- end of BOTTOM PAGER --->
+            <!--- Row Count --->
+            <div class="hidden">
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="RowCount" name="RowCount" value="<?= $Form->RowCount?>">
+                </div>
+            </div>
+
+            <!--- SelectedIndexRow --->
+            <div class="hidden">
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="SelectedIndexRowTemp" name="SelectedIndexRowTemp" value="<?= $Form->SelectedIndexRow?>">
+                </div>
+            </div>
+
+            <!--- Filter Action --->
+            <div class="hidden">
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="FilterSubmit" name="FilterSubmit" value="0">
+                </div>
+            </div>
+
+            <!--- Reset Action --->
+            <div class="hidden">
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="Reset" name="Reset" value="false">
+                </div>
+            </div>
+        </form>
+    </div> <!--- container --->
+</div>
+
+<script>
+    var PageSize = <?= $Form->PageSize; ?>;
+    var WorkstateTypeID = <?= $Obj->WorkstateTypeID; ?>;
+
+    console.log("PageSize: ", PageSize);
+    console.log("WorkstateTypeID: ", WorkstateTypeID);
+    // Show Filter if Active
+    if ((PageSize != 20) || (WorkstateTypeID != 0)) {
+        //$('#collapsibleSection').show();
+        $('#collapsibleSection').addClass('in');
+    }
+
+    //////////////////// Paging ////////////////////
+
+    // PageSize
+    $('#PageSize').on('change', function () {
+        $('#FilterSubmit').val(1);
+        this.form.submit();
+    });
+
+    // Selected Index Row
+    $('#SelectedIndexRow2').on('change', function () {
+        $('#FilterSubmit').val(1);
+        this.form.submit();
+    });
+
+    // First2 using click
+    $('#First2').on('click', function () {
+        console.log('test click');
+        $('#FilterSubmit').val(1);
+    });
+
+    // Prev2 using click
+    $('#Prev2').on('click', function () {
+        console.log('test click');
+        $('#FilterSubmit').val(1);
+    });
+
+    // Next2 using click
+    $('#Next2').on('click', function () {
+        console.log('test click');
+        $('#FilterSubmit').val(1);
+    });
+
+    // Last2 using click
+    $('#Last2').on('click', function () {
+        console.log('test click');
+        $('#FilterSubmit').val(1);
+    });
+
+    //////////////////// End Paging ////////////////////
+
+    //////////////////// Searching and Reset ////////////////////
+
+    // Search using click
+    $('#fSearchbtn').on('click',function(){
+        console.log('test click');
+        $('#FilterSubmit').val(1);
+        this.form.submit();
+    });
+
+    // Search using enter
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            $('#FilterSubmit').val(1);
+            this.form.submit();
+        }
+    });
+
+    // Reset using click
+    $('#fResetbtn').on('click', function () {
+        $('#Reset').val("true");
+        this.form.submit();
+    });
+
+    //////////////////// End Searching and Reset ////////////////////
+
+    //////////////////// Filter ////////////////////
+
+    // Filter change
+    $('#WorkstateTypeID').on('change', function () {
+        $('#FilterSubmit').val(1);
+        this.form.submit();
+    });
+    //////////////////// End Filter ////////////////////
+</script>
